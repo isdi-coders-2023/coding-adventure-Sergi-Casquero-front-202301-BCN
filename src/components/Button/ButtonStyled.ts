0c461210c8b134fdc54a7e3ee5158f4d07ccdc89
Sergi Casquero <sergicasquero@gmail.com>
2023-02-27 16:25:52 +0100

    Add Button styles

---
+++ src/components/Button/ButtonStyled.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import "@fontsource/saira-stencil-one";
+
+const ButtonStyled = styled.button`
+  width: 323px;
+  height: 40px;
+  background-color: #cccf18;
+  font-family: "saira stencil one", sans-serif;
+  font-size: 1.25rem;
+  border: none;
+  border-radius: 10px;
+`;
+
+export default ButtonStyled;
