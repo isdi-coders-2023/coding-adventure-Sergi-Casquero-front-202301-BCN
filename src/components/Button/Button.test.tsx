5fc0e6f85ad2f0330f7ad09a5d48713803d9a7b1
Sergi Casquero <sergicasquero@gmail.com>
2023-02-28 12:11:05 +0100

    Add theme provider to Button test

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,12 +1,18 @@
 import { render, screen } from "@testing-library/react";
+import { ThemeProvider } from "styled-components";
+import theme from "../../styles/theme";
 import Button from "./Button";
 
 describe("Given a Button component", () => {
-  describe("When rendered with a text", () => {
-    test("Then it should show a button on the screen", () => {
-      const buttonText = "Prueba";
+  describe("When rendered with the text 'Sample'", () => {
+    test("Then it should show a button on the screen containing that text", () => {
+      const buttonText = "Sample";
 
-      render(<Button text={buttonText} />);
+      render(
+        <ThemeProvider theme={theme}>
+          <Button text={buttonText} />
+        </ThemeProvider>
+      );
       const button = screen.getByRole("button", { name: buttonText });
 
       expect(button).toBeInTheDocument();
